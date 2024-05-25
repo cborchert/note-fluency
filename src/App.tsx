105467c406b0d21c4ac3f65936ad782e094c7ff3
Chris Borchert <christopher.borchert@datadoghq.com>
2024-05-25 14:06:22 -0400

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -25,28 +25,28 @@ const getNoteNameAtInterval = (
   const noteIndex = getNoteIndex(noteName);
   const newNoteNames = NOTE_NAMES.at(
     (noteIndex + interval) % NOTE_NAMES.length
-  );
+  ) || [];
   if (interval < 0) {
     return newNoteNames.toReversed();
   }
   return newNoteNames;
 };
 
-const CHROMATIC_INTERVALS = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-const MAJOR_INTERVALS = [2, 2, 1, 2, 2, 2, 1];
-const MINOR_INTERVALS = [2, 1, 2, 2, 1, 2, 2];
+// const CHROMATIC_INTERVALS = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+// const MAJOR_INTERVALS = [2, 2, 1, 2, 2, 2, 1];
+// const MINOR_INTERVALS = [2, 1, 2, 2, 1, 2, 2];
 
-const getScale = (noteName: string, intervals: number[]): string[][] => {
-  let noteIndex = getNoteIndex(noteName);
-  const scale = [NOTE_NAMES[noteIndex]];
-  intervals.forEach((interval) => {
-    const currentNoteName = NOTE_NAMES[noteIndex][0];
-    const newNoteNames = getNoteNameAtInterval(currentNoteName, interval);
-    scale.push(newNoteNames);
-    noteIndex = getNoteIndex(newNoteNames[0]);
-  });
-  return scale;
-};
+// const getScale = (noteName: string, intervals: number[]): string[][] => {
+//   let noteIndex = getNoteIndex(noteName);
+//   const scale = [NOTE_NAMES[noteIndex]];
+//   intervals.forEach((interval) => {
+//     const currentNoteName = NOTE_NAMES[noteIndex][0];
+//     const newNoteNames = getNoteNameAtInterval(currentNoteName, interval);
+//     scale.push(newNoteNames);
+//     noteIndex = getNoteIndex(newNoteNames[0]);
+//   });
+//   return scale;
+// };
 
 const formatNote = (notes: string[], join: boolean = true): string => {
   return join ? notes.join("/") : notes[0]
